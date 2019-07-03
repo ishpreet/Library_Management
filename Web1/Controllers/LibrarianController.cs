218f52ab717ad6d43cfc13202b3d4af08808769e
Ishpreet Dhingra <isdhingr@microsoft.com>
2019-07-03 17:42:35 +0530

    First Commit

---
+++ Web1/Controllers/LibrarianController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web1.Controllers
+{
+    public class LibrarianController : Controller
+    {
+
+        public IActionResult LoginL()
+        {
+            return View();
+        }
+    }
+}
